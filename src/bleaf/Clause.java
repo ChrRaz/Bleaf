ebbdc8607f4ec9bb625a8f7ada4fc33095fc5a6e
Ybionz <ybionz@gmail.com>
2019-05-23 10:57:59 +0200

    containsComplementary added and spelling changed

--- src/bleaf/Clause.java
+++ src/bleaf/Clause.java
@@ -5,7 +5,8 @@
  */
 package bleaf;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -13,44 +14,59 @@ import java.util.ArrayList;
  */
 public class Clause {
 
-    private ArrayList<Literal> Litterals = new ArrayList<Literal>();
-    private ArrayList<Literal> LitteralsNeg = new ArrayList<Literal>();
+    private Set<Literal> posList = new HashSet<Literal>();
+    private Set<Literal> negList = new HashSet<Literal>();
+
 
     public Clause() {
     }
 
-    public void Add(Literal litt) {
-        Litterals.add(litt);
+    public void add(Literal litt) {
+        posList.add(litt);
     }
 
-    public void AddNeg(Literal litt) {
-        LitteralsNeg.add(litt);
+    public void addNeg(Literal litt) {
+        negList.add(litt);
     }
 
+    public boolean containsComplementary(Clause c) {
+        for (Literal litt : c.posList) {
+            if (this.negList.contains(litt)) {
+                return true;
+            }
+        }
+        for (Literal litt : c.negList) {
+            if (this.posList.contains(litt)) {
+                return true;
+            }
+        }
+        return false;
+    }
+/*
     public void Print() {
         System.out.print("{");
-        for (int i = 0; i < Litterals.size(); i++) {
-            if (i == Litterals.size() - 1) {
-                Litterals.get(i).Print();
-                if(LitteralsNeg.size()>0){
+        for (int i = 0; i < posList.size(); i++) {
+            if (i == posList.size() - 1) {
+                posList.get(i).Print();
+                if(negList.size()>0){
                     System.out.print(",");
                 }
             } else {
-                Litterals.get(i).Print();
+                posList.get(i).Print();
                 System.out.print(",");
             }
 
         }
-        for (int i = 0; i < LitteralsNeg.size(); i++) {
+        for (int i = 0; i < negList.size(); i++) {
             System.out.print("-");
-            if (i == LitteralsNeg.size() - 1) {
-                LitteralsNeg.get(i).Print();
+            if (i == negList.size() - 1) {
+                negList.get(i).Print();
             } else {
-                LitteralsNeg.get(i).Print();
+                negList.get(i).Print();
                 System.out.print(",");
             }
         }
         System.out.print("}");
     }
-
+*/
 }
