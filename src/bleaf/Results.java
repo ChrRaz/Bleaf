d3f226a9de5d6795ea066d56c0ca96370122e816
ChrRaz <chr@raznet.dk>
2019-05-29 14:19:25 +0200

    Clean up UI

--- src/bleaf/Results.java
+++ src/bleaf/Results.java
@@ -5,27 +5,30 @@ import java.util.Set;
 public class Results {
 
     public static void showResolution(ClauseSet KB, ClauseSet phi) {
-        System.out.println("KB");
-        System.out.println(KB);
+        System.out.println();
+        System.out.println("The original belief base was " + KB);
 
-        System.out.println("Resolution");
-        System.out.println(KB.resolution(new ClauseSet(phi)));
-
+        boolean res = KB.resolution(new ClauseSet(phi));
+        System.out.println("The belief base " + (!res ? "entails" : "does not entail") + " phi");
+        System.out.println();
     }
-    
+
     public static void showRemainders(ClauseSet KB, ClauseSet phi) {
-        System.out.println("KB");
-        System.out.println(KB);
-        
+        System.out.println();
+        System.out.println("The original belief base was " + KB);
+
         Set<ClauseSet> remset = KB.remainders(new ClauseSet(phi));
         Set<ClauseSet> gammaremset = ClauseSet.gamma(remset);
+        // Compute intersection
         ClauseSet intersection = new ClauseSet(KB);
         for (ClauseSet rem : gammaremset) {
             intersection.clauseList.retainAll(rem.clauseList);
         }
 
-        System.out.println("Intersections:");
-        System.out.println(intersection);
+        System.out.println("The remainders are " + intersection);
+
+        System.out.println("The contraction of the belief base is " + intersection);
+        System.out.println();
     }
 
 }
